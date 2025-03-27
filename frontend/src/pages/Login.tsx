f45b8d1cfdc1eceed5185614282666f52d8bd6ae
Ismail <ismailabdullah1129@gmail.com>
2025-03-26 21:03:36 -0700

    feat: add logo image to Login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -35,7 +35,7 @@ function Login() {
           <div>
             <img
               className="h-12 w-auto mx-auto"
-              src=""
+              src="frontend/src/team-tasks-ai-logo.png"
               alt="Team Tasks AI Icon/Logo"
             />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
